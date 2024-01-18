5564aa13c6e0b9f65d45dbd10cae46aad9805459
Laqwanda Nettles <nettleslaqwanda@gmail.com>
2024-01-17 21:29:11 -0600

    created js & css files and linked them. adjusted numbers in index

---
+++ script.js
@@ -0,0 +1,33 @@
+document.querySelectorAll("area").forEach((area) => {
+  area.addEventListener("mouseenter", () => {
+    const coords = area.coords.split(",").map((coord) => parseInt(coord, 10));
+    const shape = area.getAttribute("shape");
+    const highlightDiv = document.createElement("div");
+
+    highlightDiv.className = "highlight-area";
+
+    if (shape === "rect") {
+      const [left, top, right, bottom] = coords;
+      highlightDiv.style.left = `${left}px`;
+      highlightDiv.style.top = `${top}px`;
+      highlightDiv.style.width = `${right - left}px`;
+      highlightDiv.style.height = `${bottom - top}px`;
+    } else if (shape === "circle") {
+      const [x, y, radius] = coords;
+      highlightDiv.style.left = `${x - radius}px`;
+      highlightDiv.style.top = `${y - radius}px`;
+      highlightDiv.style.width = `${radius * 2}px`;
+      highlightDiv.style.height = `${radius * 2}px`;
+      highlightDiv.style.borderRadius = "50%";
+    }
+
+    document.getElementById("dinner").parentNode.appendChild(highlightDiv);
+    area.addEventListener(
+      "mouseleave",
+      () => {
+        highlightDiv.remove();
+      },
+      { once: true }
+    );
+  });
+});
